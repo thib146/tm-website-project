2095e7efd8e175a5e5859849f20ad94e0e2157e2
Thibaut Massard <thibaut.massard@gadz.org>
2016-12-08 19:06:33 +0100

    Add files via upload

--- js/thibautmassard.js
+++ js/thibautmassard.js
@@ -1,4 +1,4 @@
-// Thibaut Massard - JavaScript functions
+// Thibaut Massard - JavaScript
 
 (function($) {
   "use strict"; // Start of use strict
@@ -35,7 +35,7 @@
     }
   })
 
-  // Reveal stuff while scrolling
+  // Images and charts revealing while scrolling - using scrollreveal.js
   window.sr = ScrollReveal();
   sr.reveal('.btnreveal', {delay:4700});
   sr.reveal('.image1', {delay:300});
@@ -48,8 +48,14 @@
   sr.reveal('.cpp', {delay:500});
   sr.reveal('.javascript', {delay:700});
   sr.reveal('.html', {delay:900});
+  sr.reveal('.lfr', {delay:300});
+  sr.reveal('.len', {delay:500});
+  sr.reveal('.lsp', {delay:700});
+  sr.reveal('.mjava', {delay:300});
+  sr.reveal('.mweb', {delay:500});
+  sr.reveal('.mgame', {delay:700});
   
-  // Initialize a Line chart in the container with the ID chart1
+  // HTML/CSS Chart
   var chart1 = new Chartist.Pie('#chart1', {
     labels: ['60%'],
     series: [60]
@@ -59,7 +65,7 @@
     startAngle: 270,
     total: 200,
   });
-  // Initialize a Line chart in the container with the ID chart2
+  // Javascript Chart
   var chart2 = new Chartist.Pie('#chart2', {
     labels: ['40%'],
     series: [40]
@@ -69,7 +75,7 @@
     startAngle: 270,
     total: 200,
   });
-  // Initialize a Line chart in the container with the ID chart3
+  // C++ Chart
   var chart3 = new Chartist.Pie('#chart3', {
     labels: ['50%'],
     series: [50]
@@ -79,7 +85,7 @@
     startAngle: 270,
     total: 200,
   });
-  // Initialize a Line chart in the container with the ID chart2
+  // Java Chart
   var chart4 = new Chartist.Pie('#chart4', {
     labels: ['50%'],
     series: [50]
@@ -89,6 +95,7 @@
     startAngle: 270,
     total: 200,
   });
+  // French Chart
   var chartfr = new Chartist.Pie('#chartfr', {
     labels: ['100%'],
     series: [100]
@@ -98,6 +105,7 @@
     startAngle: 270,
     total: 200,
   });
+  // English Chart
   var charten = new Chartist.Pie('#charten', {
     labels: ['100%'],
     series: [100]
@@ -107,6 +115,7 @@
     startAngle: 270,
     total: 200,
   });
+  // Spanish Chart
   var chartsp = new Chartist.Pie('#chartsp', {
     labels: ['50%'],
     series: [50]
@@ -116,8 +125,11 @@
     startAngle: 270,
     total: 200,
   });
-
-  chart1.on('draw', function(data) {
+  
+  // General function to draw the charts with an animation
+  function drawChartTM (chart)
+  {
+	chart.on('draw', function(data) {
     if(data.type === 'slice') {
       // Get the total path length in order to use for dash array animation
       var pathLength = data.element._node.getTotalLength();
@@ -148,318 +160,55 @@
       // We can't use guided mode as the animations need to rely on setting begin manually
       // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
       data.element.animate(animationDefinition, false);
-    }
-  });
-  chart2.on('draw', function(data) {
-    if(data.type === 'slice') {
-      var pathLength = data.element._node.getTotalLength();
-      data.element.attr({
-        'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-      });
-      var animationDefinition = {
-        'stroke-dashoffset': {
-          id: 'anim' + data.index,
-          dur: 2000,
-          from: -pathLength + 'px',
-          to:  '0px',
-          easing: Chartist.Svg.Easing.easeOutQuint,
-          fill: 'freeze'
-        }
-      };
-      if(data.index !== 0) {
-        animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-      }
-      data.element.attr({
-        'stroke-dashoffset': -pathLength + 'px'
-      });
-      data.element.animate(animationDefinition, false);
-    }
-  });
-  chart3.on('draw', function(data) {
-    if(data.type === 'slice') {
-      var pathLength = data.element._node.getTotalLength();
-      data.element.attr({
-        'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-      });
-      var animationDefinition = {
-        'stroke-dashoffset': {
-          id: 'anim' + data.index,
-          dur: 2000,
-          from: -pathLength + 'px',
-          to:  '0px',
-          easing: Chartist.Svg.Easing.easeOutQuint,
-          fill: 'freeze'
-        }
-      };
-      if(data.index !== 0) {
-        animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-      }
-      data.element.attr({
-        'stroke-dashoffset': -pathLength + 'px'
-      });
-      data.element.animate(animationDefinition, false);
-    }
-  });
-  chart4.on('draw', function(data) {
-    if(data.type === 'slice') {
-      var pathLength = data.element._node.getTotalLength();
-      data.element.attr({
-        'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-      });
-      var animationDefinition = {
-        'stroke-dashoffset': {
-          id: 'anim' + data.index,
-          dur: 2000,
-          from: -pathLength + 'px',
-          to:  '0px',
-          easing: Chartist.Svg.Easing.easeOutQuint,
-          fill: 'freeze'
-        }
-      };
-      if(data.index !== 0) {
-        animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-      }
-      data.element.attr({
-        'stroke-dashoffset': -pathLength + 'px'
-      });
-      data.element.animate(animationDefinition, false);
-    }
-  });
-  chartfr.on('draw', function(data) {
-    if(data.type === 'slice') {
-      var pathLength = data.element._node.getTotalLength();
-      data.element.attr({
-        'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-      });
-      var animationDefinition = {
-        'stroke-dashoffset': {
-          id: 'anim' + data.index,
-          dur: 2000,
-          from: -pathLength + 'px',
-          to:  '0px',
-          easing: Chartist.Svg.Easing.easeOutQuint,
-          fill: 'freeze'
-        }
-      };
-      if(data.index !== 0) {
-        animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-      }
-      data.element.attr({
-        'stroke-dashoffset': -pathLength + 'px'
-      });
-      data.element.animate(animationDefinition, false);
-    }
-  });
-  charten.on('draw', function(data) {
-    if(data.type === 'slice') {
-      var pathLength = data.element._node.getTotalLength();
-      data.element.attr({
-        'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-      });
-      var animationDefinition = {
-        'stroke-dashoffset': {
-          id: 'anim' + data.index,
-          dur: 2000,
-          from: -pathLength + 'px',
-          to:  '0px',
-          easing: Chartist.Svg.Easing.easeOutQuint,
-          fill: 'freeze'
-        }
-      };
-      if(data.index !== 0) {
-        animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-      }
-      data.element.attr({
-        'stroke-dashoffset': -pathLength + 'px'
-      });
-      data.element.animate(animationDefinition, false);
-    }
-  });
-  chartsp.on('draw', function(data) {
-    if(data.type === 'slice') {
-      var pathLength = data.element._node.getTotalLength();
-      data.element.attr({
-        'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-      });
-      var animationDefinition = {
-        'stroke-dashoffset': {
-          id: 'anim' + data.index,
-          dur: 2000,
-          from: -pathLength + 'px',
-          to:  '0px',
-          easing: Chartist.Svg.Easing.easeOutQuint,
-          fill: 'freeze'
-        }
-      };
-      if(data.index !== 0) {
-        animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-      }
-      data.element.attr({
-        'stroke-dashoffset': -pathLength + 'px'
-      });
-      data.element.animate(animationDefinition, false);
-    }
-  });
-
-  /*
-  // Load the Visualization API and the corechart package.
-  google.charts.load('current', {'packages':['corechart']});
-
-
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
-  function drawChartProg() {
-    // Create the data table.
-    var data = new google.visualization.arrayToDataTable([
-      ['Language', 'Percentage', { role: 'style' }, {role: 'annotation'}],
-      ['HTML5/CSS3', 60, 'color:#f14a29; opacity:0.8;', '60%'],
-      ['JavaScript', 40, 'color:#63a814; opacity:0.8;', '40%'],
-      ['C++', 50, 'color:#0055a4; opacity:0.8;', '50%'],
-      ['VB', 60, 'color:#683591; opacity:0.8;', '60%'],
-    ]);
-
-    // Set chart options
-    var options = {backgroundColor: 'transparent',
-    animation: {startup: true, duration: 1000, easing: 'out'},
-    tooltip: {trigger: 'none'},
-    annotations: {textStyle: {fontName: "Montserrat"}},
-    hAxis: {
-      gridlines: {color: 'none'},
-      textStyle: {color: 'none'},
-      minValue:0, maxValue: 100},
-      vAxis: {
-        textStyle: {color: 'white', fontName: "Montserrat"},
-      },
-      bar: {groupWidth: "40%"},
-      legend: 'none',
-      width:650,
-      height:300};
-
-      // Instantiate and draw our chart, passing in some options.
-      var container = document.getElementById('chart_div');
-      var chart = new google.visualization.BarChart(container);     */
-
-      /*var colors = ['#f14a29', '#63a814', '#0055a4', '#683591'];
-
-      google.visualization.events.addListener(chart, 'ready', changeBorderRadius);
-      google.visualization.events.addListener(chart, 'select', changeBorderRadius);
-      google.visualization.events.addListener(chart, 'onmouseover', changeBorderRadius);
-      google.visualization.events.addListener(chart, 'onmouseout', changeBorderRadius);
-
-      function changeBorderRadius() {
-      var chartColumns = container.getElementsByTagName('rect');
-      Array.prototype.forEach.call(chartColumns, function(column) {
-      if ((colors.indexOf(column.getAttribute('fill')) > -1) ||
-      (column.getAttribute('fill') === 'none') ||
-      (column.getAttribute('stroke') === '#ffffff')) {
-      column.setAttribute('rx', 15);
-      column.setAttribute('ry', 15);
-    }
-  });
-}
-
-//chart.draw(data, options);
-}*/
-
-/*function drawChartLanguages() {
-  // Create the data table.
-  var data = new google.visualization.arrayToDataTable([
-    ['Language', 'P1', { role: 'style' }, 'P2', { role: 'style' }, 'P3', { role: 'style' }, {role: 'annotation'}],
-    ['French',   100, 'color:#EF4135; opacity:0.8;', 100, 'color:#FFFFFF; opacity:0.8;', 100, 'color:#0055A4; opacity:0.8;', '100%'],
-    ['English',  100, 'color:#002868; opacity:0.8;', 100, 'color:#002868; opacity:0.8;', 100, 'color:#002868; opacity:0.8;', '100%'],
-    ['Spanish',   50, 'color:#AA151B; opacity:0.8;',  50, 'color:#F1BF00; opacity:0.8;',  50, 'color:#AA151B; opacity:0.8;',  '50%']
-  ]);
-
-  // Set chart options
-  var options = {
-    backgroundColor: 'transparent',
-    animation: {startup: true, duration: 1000, easing: 'out'},
-    annotations: {textStyle: {fontName: "Montserrat"}},
-    tooltip: {trigger: 'none'},
-    isStacked: true,
-    hAxis: {
-      gridlines: {color: 'none'},
-      textStyle: {color: 'none'},
-      direction: 1,
-      minValue:0, maxValue: 100},
-      vAxes: {
-        0: {textPosition: 'right'},
-        1: {},
-      },
-      vAxis: {textStyle: {color: 'white', fontName: "Montserrat"}},
-      bar: {groupWidth: "40%"},
-      legend: 'none',
-      width:670,
-      height:220};
-
-      // Instantiate and draw our chart, passing in some options.
-      var container = document.getElementById('chart_div2');
-      var chart = new google.visualization.BarChart(container);
-
-      chart.draw(data, options);
-      //chart.draw(data, google.charts.Bar.convertOptions(options));
-    }*/
-
-    // Draw/animate the charts when the user reaches "Charts"
-    /*$(document).bind('scroll', function(ev) {
-      var scrollOffset = $(document).scrollTop();
-      var containerOffset = $('#charts').offset().top - window.innerHeight;
-      if (scrollOffset > containerOffset) {
-        drawChartProg();
-        drawChartLanguages();
-        // unbind event not to load scrolls again
-        $(document).unbind('scroll');
-      }
-    });*/
-
-    /* Text animations - Story section
-    window.onscroll = function() {moveRightPrepaText2()};
-    function moveRightPrepaText2() {
-      if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-        document.getElementById('prepa').className = "slideRight";
-        document.getElementById('prepa2').className = "slideRight";
-      }
-      if (document.body.scrollTop > 1300 || document.documentElement.scrollTop > 1300) {
-        document.getElementById('am').className = "slideLeft";
-        document.getElementById('am2').className = "slideLeft";
-      }
-      if (document.body.scrollTop > 1900 || document.documentElement.scrollTop > 1900) {
-        document.getElementById('safran').className = "slideRight";
-        document.getElementById('safran2').className = "slideRight";
-      }
-      if (document.body.scrollTop > 2300 || document.documentElement.scrollTop > 2300) {
-        document.getElementById('iac').className = "slideLeft";
-        document.getElementById('iac2').className = "slideLeft";
-      }
-      if (document.body.scrollTop > 2700 || document.documentElement.scrollTop > 2700) {
-        document.getElementById('trexia').className = "slideRight";
-        document.getElementById('trexia2').className = "slideRight";
-      }
-    }*/
-	
-	window.onscroll = function() {borders()};
-    function borders() {
-      if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-        document.getElementById('ib1').className += " intro-border1-animation";
-        document.getElementById('ib2').className += " intro-border2-animation";
-        document.getElementById('ib3').className += " intro-border3-animation";
-        document.getElementById('ib4').className += " intro-border4-animation";
-        document.getElementById('ib5').className += " intro-border5-animation";
-      }
 	}
+	});
+  }
+  
+  // Draw all the charts
+  drawChartTM(chart1);
+  drawChartTM(chart2);
+  drawChartTM(chart3);
+  drawChartTM(chart4);
+  drawChartTM(chartfr);
+  drawChartTM(charten);
+  drawChartTM(chartsp);
+	
+  // Border reveal throughout the page, depending on the window level in px
+  window.onscroll = function() {borders()};
+  function borders() {
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+	  document.getElementById('ib1').className += " intro-border1-animation";
+	  document.getElementById('ib2').className += " intro-border2-animation";
+	  document.getElementById('ib3').className += " intro-border3-animation";
+	  document.getElementById('ib4').className += " intro-border4-animation";
+	  document.getElementById('ib5').className += " intro-border5-animation";
+	  document.getElementById('ib6').className += " intro-border6-animation";
+	  document.getElementById('ib7').className += " intro-border7-animation";
+	  document.getElementById('ib8').className += " intro-border8-animation";
+	  document.getElementById('ib9').className += " intro-border9-animation";
+	  document.getElementById('ib10').className += " intro-border10-animation";
+	  document.getElementById('ib11').className += " intro-border11-animation";
+    }
+    if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+	  document.getElementById('sb1').className += " story-border1-animation";
+	  document.getElementById('sb2').className += " story-border2-animation";
+	  document.getElementById('sb3').className += " story-border3-animation";
+	  document.getElementById('sb4').className += " timeline-animation";
+	  document.getElementById('sb5').className += " story-border5-animation";
+    }
+  }
 
-    // Background scrolls speeds
-    $("#parallax").each(function() {
-      $scroll_speed = 5;
-      $thisi = $(this);
-      $(window).scroll(function() {
-        $bgScroll = -(($(window).scrollTop() - $thisi.offset().top) / $scroll_speed);
-        $bgPosition = 'center ' + $bgScroll + 'px';
-        $thisi.css({
-          backgroundPosition: $bgPosition
-        });
-      });
+  // Background scrolls speeds
+  $("#parallax").each(function() {
+    $scroll_speed = 5;
+    $thisi = $(this);
+    $(window).scroll(function() {
+	  $bgScroll = -(($(window).scrollTop() - $thisi.offset().top) / $scroll_speed);
+	  $bgPosition = 'center ' + $bgScroll + 'px';
+	  $thisi.css({
+	    backgroundPosition: $bgPosition
+	  });
     });
+  });
 
-  })(jQuery); // End of use strict
+})(jQuery); // End of use strict
